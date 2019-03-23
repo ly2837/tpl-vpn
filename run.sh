c2ba40662211a635e9f6095e567b3954d96fe1ac
ali <liuyang@ly2837.com>
2019-03-23 12:44:34 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+apt update
+apt install -f docker.io
+xz -d -k vpn.tar.gz
+docker load -i vpn.tar
+docker run \
+    --name ipsec-vpn-server \
+    --env-file ./vpn.env \
+    --restart=always \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
